49d131d37af82adaa9c03e03ad696766faa6eecd
Ignacio Aparicio <iaparicio@redvelgames.com>
2021-01-30 10:47:25 +0100

    Modelo de Deck (Jugador)

---
+++ Assets/Scripts/DataModels/DeckDataModel/DeckDataModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrueColors.Data
+{
+    [Serializable]
+    public class DeckDataModel
+    {
+        public List<CardDataModelWrapper> cards;
+    }
+}
